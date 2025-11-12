bb4d3d39643f48ad1640cd303dcfc475f38f0231
Helga09 <104345657+Helga09@users.noreply.github.com>
2025-11-12 21:01:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0'; 
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end('<h1>Веб-сервер на Node.js!</h1>');
+
+    res.end('<h1>Node.js UI!</h1>');
 });
 
-server.listen(PORT, () => {
-    console.log(`Сервер працює на: http://localhost:${PORT}/`);
+server.listen(PORT, HOST, () => {
+    console.log(`Сервер працює на: http://${HOST}:${PORT}/`);
+    console.log('Готовий до розгортання з Nixpacks!');
 });
